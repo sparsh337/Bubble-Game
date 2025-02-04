36375a3438a3d9e64deeec59bdf162cad13bdea6
Sparsh Doshi <123226042+sparsh337@users.noreply.github.com>
2025-02-04 21:44:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function makebubble(){
+    
+let bubbly = " ";
+
+for(let i =1; i<=154;i++){
+    let rn = Math.floor(Math.random()*10);
+    bubbly += `<div class="bubble">${rn}</div>`;
+}
+document.querySelector("#pbtm").innerHTML = bubbly;
+
+
+}
+makebubble();
+
+var timer = 60;
+function runtimer() {
+    var interval = setInterval(() => {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timervalue").textContent = timer;
+        } else {
+            clearInterval(interval); // Stop the timer when it reaches 0
+        }
+    }, 1000);
+}
+runtimer();
+function hitnumber(){
+    hitnum=Math.floor(Math.random()*10);
+    document.querySelector("#hit").textContent =hitnum  ;
+}
+
+
+
+var score = 0;
+function increasescore(){
+
+    score+=1;
+    document.querySelector("#scorevalue").textContent=score;
+}
+
+
+document.querySelector("#pbtm").addEventListener("click",function(e){
+    console.log(e.target.innerText )
+    let hitval =document.querySelector("#hit").innerText;
+    if(e.target.innerText == hitval){
+        increasescore();
+        hitnumber();
+    }
+    makebubble()
+});
